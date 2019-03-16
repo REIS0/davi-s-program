ec5ad9c5e95436ea0cf6d6468077b3fc00ce467a
Maruseu <marceucamp@gmail.com>
2019-03-16 14:27:07 -0300

    davi for linux™

---
+++ davi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo '--------------------------------'
+echo 'DAVI TEXT GENERATOR'
+echo '--------------------------------'
+
+read -p 'O que voce acha fracassado? ' nomes1
+read -p 'O que essa coisa é? ' nomes2
+
+echo '
+Eu acho o '$nomes1' uma '$nomes2' muito fracassado.
+no fundo ela so quer ajudar todo mundo e pipipi popopo
+mas TODA acao dela consegue fuder com tudo mais do que ja tava
+'
