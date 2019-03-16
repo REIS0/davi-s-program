2ce70da45d78c82322b77331ca3ac99103d2a469
Maruseu <marceucamp@gmail.com>
2019-03-16 14:24:50 -0300

    davi but in python 2

---
+++ davi(py2).py
@@ -0,0 +1,10 @@
+print '-' * 30
+print 'DAVI TEXT GENERATOR'
+print '-' * 30
+nomes1=raw_input('O que voce acha fracassado? ')
+nomes2=raw_input('O que essa coisa eh? ')
+print '''
+\nEu acho o %s uma %s muito fracassado.
+no fundo ela so quer ajudar todo mundo e pipipi popopo
+mas TODA acao dela consegue fuder com tudo mais do que ja tava
+''' % (nomes1, nomes2)
